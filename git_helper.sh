898f32ce3b94b083a30b3e5365c955a856ae4f32
Andre Winkler <gluehloch@googlemail.com>
2014-03-08 15:15:30 +0100

    FIX: Suppress 'this is not a GIT repository' messages.

--- git_helper.sh
+++ git_helper.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 function git_deleted {
-	[[ $(git status 2> /dev/null | grep deleted:) != "" ]] && echo "-"
+    [[ $(git status 2> /dev/null | grep deleted:) != "" ]] && echo "-"
 }
 
 function git_added {
-	[[ $(git status 2> /dev/null | grep "Untracked files:") ]] && echo "+"
+    [[ $(git status 2> /dev/null | grep "Untracked files:") ]] && echo "+"
 }
 
 function git_modified {
-	[[ $(git status 2> /dev/null | grep "modified:") != "" ]] && echo "*"
+    [[ $(git status 2> /dev/null | grep "modified:") != "" ]] && echo "*"
 }
 
 function git_dirty {
-	echo "$(git_added)$(git_modified)$(git_deleted)"
+    echo "$(git_added)$(git_modified)$(git_deleted)"
 }
 
 function git_branch {
-    git branch | sed -n '/\* /s///p'
-	###git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' 
-	###-e "s/* \(.*\)/[\l$(git_dirty)]/"
-	###
+    git branch --no-color 2> /dev/null | sed -n '/\* /s///p'
+    ###git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' 
+    ###-e "s/* \(.*\)/[\l$(git_dirty)]/"
+    ###
 }
 
 export PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\] [$(git_branch)] [$(git_dirty)] \n$ '
@@ -63,43 +63,43 @@ function git_info() {
 }
 
 function git_stats {
-	if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
-		echo "Number of commits per author:"
-		git --no-pager shortlog -sn --all
-		AUTHORS=$( git shortlog -sn --all | cut -f2 | cut -f1 -d' ')
-		LOGOPTS=""
-		if [ "$1" == '-w' ]; then
-			LOGOPTS="$LOGOPTS -w"
-			shift
-		fi
-		if [ "$1" == '-M' ]; then
-			LOGOPTS="$LOGOPTS -M"
-			shift
-		fi
-		if [ "$1" == '-C' ]; then
-			LOGOPTS="$LOGOPTS -C --find-copies-harder"
-			shift
-		fi
-		for a in $AUTHORS
-		do
-			echo '-------------------'
-			echo "Statistics for: $a"
-			echo -n "Number of files changed: "
-			git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f3 | sort -iu | wc -l
-			echo -n "Number of lines added: "
-			git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
-			echo -n "Number of lines deleted: "
-			git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
-			echo -n "Number of merges: "
-			git log $LOGOPTS --all --merges --author=$a | grep -c '^commit'
-		done
-	else
-		echo "you're currently not in a git repository"
-	fi
+    if [ -n "$(git symbolic-ref HEAD 2> /dev/null)" ]; then
+        echo "Number of commits per author:"
+        git --no-pager shortlog -sn --all
+        AUTHORS=$( git shortlog -sn --all | cut -f2 | cut -f1 -d' ')
+        LOGOPTS=""
+        if [ "$1" == '-w' ]; then
+            LOGOPTS="$LOGOPTS -w"
+            shift
+        fi
+        if [ "$1" == '-M' ]; then
+            LOGOPTS="$LOGOPTS -M"
+            shift
+        fi
+        if [ "$1" == '-C' ]; then
+            LOGOPTS="$LOGOPTS -C --find-copies-harder"
+            shift
+        fi
+        for a in $AUTHORS
+        do
+            echo '-------------------'
+            echo "Statistics for: $a"
+            echo -n "Number of files changed: "
+            git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f3 | sort -iu | wc -l
+            echo -n "Number of lines added: "
+            git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f1 | awk '{s+=$1} END {print s}'
+            echo -n "Number of lines deleted: "
+            git log $LOGOPTS --all --numstat --format="%n" --author=$a | cut -f2 | awk '{s+=$1} END {print s}'
+            echo -n "Number of merges: "
+            git log $LOGOPTS --all --merges --author=$a | grep -c '^commit'
+        done
+    else
+        echo "you're currently not in a git repository"
+    fi
 }
 
 function git_remove_missing_files() {
-	git ls-files -d -z | xargs -0 git update-index --remove
+    git ls-files -d -z | xargs -0 git update-index --remove
 }
 
 
