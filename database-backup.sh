1e31292f8b49ab5ccf3a67dd01228a62f9be866f
Andre Winkler <gluehloch@googlemail.com>
2021-06-30 06:43:10 +0200

    UPDATE: Betoffice DB backup script.

--- database-backup.sh
+++ database-backup.sh
@@ -4,6 +4,10 @@
 rsync --delete -avzbe ssh mariadb.tdkb2:~/backup ~/tippdiekistebier
 
 databaseImport() {
+    echo "Parameter: filename-prefix=$1, user=$2, password=$3, database=$4"
+    echo "----> Start import ..."
+
+
     NEWEST_BACKUP_FILE=$(ls -t ~/tippdiekistebier/backup/$1* | head -1)
     IMPORT_FILE=${NEWEST_BACKUP_FILE%.*}
 
@@ -14,8 +18,14 @@ databaseImport() {
     # Virtual Box | betoffice development server
     # /bin/mysql -u betofficesu --password=betoffice -D betoffice -h 192.168.0.136 < "$IMPORT_FILE"
     # Docker | betofficedb
-    /bin/mysql -u $2 --password=$3 -D $4 -h 192.168.99.101 < "$IMPORT_FILE"
-    # /bin/mysql -u betofficesu --password=betoffice -D betoffice -h 127.0.0.1 < "$IMPORT_FILE"
+    /bin/mysql -u $2 --password=$3 -D $4 -h 127.0.0.1 < $IMPORT_FILE
+
+    if [ $? -eq 0 ]
+    then
+        echo "----> Import successful."
+    else
+        echo "----> ERROR: $?" 
+    fi
 }
 
 databaseImport 'betoffice' betofficesu betoffice betoffice
